508286ec701fb7c36e6996027dc6fd0221d2228e
sabra <mohamed2006fs@gmail.com>
2018-06-13 20:03:02 +0200

    Add SMS message.

--- app/src/main/java/com/example/cartracking/ControlActivity.java
+++ app/src/main/java/com/example/cartracking/ControlActivity.java
@@ -1,12 +1,19 @@
 package com.example.cartracking;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelUuid;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,10 +26,11 @@ import java.util.Set;
 
 public class ControlActivity extends AppCompatActivity {
 
-    private static int REQUEST_ENABLE_BT = 1;
-    private static String TAG = "ControlActivity";
-    private static String PHONE_NUMBER = "0020111";
-    private static String MESSAGE = "message";
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "ControlActivity";
+    private static final String PHONE_NUMBER = "0020111";
+    private static final String MESSAGE = "message";
 
     private Button authButton;
     private OutputStream outputStream;
@@ -130,7 +138,53 @@ public class ControlActivity extends AppCompatActivity {
 
     public void viewCarSMS(View view) {
         // send the sms
+        sendSMSMessage();
         // view the maps activity
+    }
+
+    protected void sendSMSMessage(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)){
+
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_SEND_SMS:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(PHONE_NUMBER, null, MESSAGE, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent", Toast.LENGTH_LONG).show();
+
+                    // or
+//                    Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+//
+//                    smsIntent.setData(Uri.parse("smsto:"));
+//                    smsIntent.setType("vnd.android-dir/mms-sms");
+//                    smsIntent.putExtra("address"  , PHONE_NUMBER);
+//                    smsIntent.putExtra("sms_body"  , MESSAGE);
+//
+//                    try {
+//                        startActivity(smsIntent);
+//                        finish();
+//                        Log.i("Finished sending SMS...", "");
+//                    } catch (android.content.ActivityNotFoundException ex) {
+//                        Toast.makeText(MainActivity.this,
+//                                "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
+//                    }
+
+                } else{
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
     public void viewCar(View view) {
